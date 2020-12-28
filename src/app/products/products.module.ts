1bd46a2b297dd4264f4017513f9bdf0b8b8d5fa1
fer <arielgeronimo517@gmail.com>
2020-12-27 22:00:07 -0600

    definiendo la plantilla para el listado de productos

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductsComponent } from './products.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { MaterialModule } from '../shared/material/material.module';
 
 
 @NgModule({
   declarations: [ProductsComponent, ProductListComponent],
   imports: [
     CommonModule,
-    ProductsRoutingModule
+    ProductsRoutingModule,
+    MaterialModule
   ]
 })
 export class ProductsModule { }
