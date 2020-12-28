e1ee894ddf2e6c048d826a296060559c35e90b3c
fer <arielgeronimo517@gmail.com>
2020-12-27 19:53:26 -0600

    creando el componente de login

---
+++ src/app/shared/components/prueba/prueba.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'st-prueba',
+  templateUrl: './prueba.component.html',
+  styleUrls: ['./prueba.component.css']
+})
+export class PruebaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material/material.module';
+import { LoginComponent } from './components/login/login.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginComponent, PruebaComponent],
   imports: [
     CommonModule,
     MaterialModule
