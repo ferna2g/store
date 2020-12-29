66791797d2c23e2dd413d296e67f2cdc253ca03c
fer <arielgeronimo517@gmail.com>
2020-12-28 19:16:32 -0600

    registrando productos en el backend

--- src/app/products/product-add/product-add.component.ts
+++ src/app/products/product-add/product-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { ProductsService } from '../shared/services/products.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'st-product-add',
@@ -16,9 +18,29 @@ export class ProductAddComponent implements OnInit {
     thumbImage: new FormControl(''),
   });
 
-  constructor() { }
+  constructor(private service: ProductsService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  submit(){
+    if(this.form.valid){
+      const product = this.form.value; //Product
+      console.log('Going to save', product);
+      this.service.add(product)
+        .subscribe(result => {
+          console.log('The product has been added', result);
+
+        })
+    }else {
+      console.error('Form is invalid');
+
+    }
+  }
+
+  cancel(){
+    this.router.navigate([''])
   }
 
 }
