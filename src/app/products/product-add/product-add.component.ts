f24fd7419b81de648ec63d637151b4184f95ef6a
fer <arielgeronimo517@gmail.com>
2020-12-28 18:58:45 -0600

    agregando el modelo de datos para el registro de productos

--- src/app/products/product-add/product-add.component.ts
+++ src/app/products/product-add/product-add.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'st-product-add',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-add.component.css']
 })
 export class ProductAddComponent implements OnInit {
+
+  form: FormGroup = new FormGroup({
+    title: new FormControl(''),
+    brand: new FormControl(''),
+    price: new FormControl(''),
+    salePrice: new FormControl(''),
+    thumbImage: new FormControl(''),
+  });
 
   constructor() { }
 
